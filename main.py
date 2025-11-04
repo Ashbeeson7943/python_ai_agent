94298859544d78aabfd0fa919f41bcdb38bd7ea2
Ashbeeson7943 <ashbeeson@hotmail.co.uk>
2025-11-04 11:11:56 +0000

    Adding functions to llm arsenal

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from config import SYSTEM_PROMPT
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+
+config=types.GenerateContentConfig(tools=[available_functions], system_instruction=SYSTEM_PROMPT)
 
 def main():
     verbose = False
@@ -28,7 +38,13 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=message_history)
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=message_history, config=config)
+    res_func_calls = response.function_calls
+    
+    if not res_func_calls == None: 
+        for function_call_part in res_func_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
     print(response.text)
     
 
